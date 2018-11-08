16375c18e15f4f0d81933523a2a390bfe30075ff
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2018-11-07 16:09:53 -0800

    Hero class added

--- superhero.py
+++ superhero.py
@@ -9,15 +9,37 @@ class Hero:
     def add_ability(self, ability):
         self.abilities.append(ability)
 
-        return self.abilities
 
     def attack(self):
+        total = 0
         for ability in self.abilities:
-            ability.attack()
+           total += ability.attack()
+        return total
+            
     
     def take_damage(self, damage):
-        self.current_health.update()
+        self.current_health -= damage
 
     def is_alive(self):
         if self.current_health > 0:
-            self.is_alive == True
+            return True
+        else:
+            return False
+
+    def fight(self, opponent):
+        while self.is_alive == True:
+        
+           pass
+
+
+if __name__ == "__main__":
+        hero = Hero("Wonder Woman")
+        print(hero.attack())
+
+class Ability:
+    def __init__ (self, name, attack_strength):
+        '''Initialize starting values'''
+        pass
+    def attack(self):
+        '''Returns attack value between 0 and full attack'''
+        pass
